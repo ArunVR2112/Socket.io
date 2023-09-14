f1fc855430b95680089d1776ee137e9801cac517
ArunVR2112 <78718321+ArunVR2112@users.noreply.github.com>
2023-09-15 01:35:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 let express = require("express");
 let app = express();
@@ -12,7 +11,7 @@ app.use(cors());
 
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:4200", // Replace with your Angular app's URL
+    origin: "http://localhost:4200", 
     methods: ["GET", "POST"],
   },
 });
@@ -31,33 +30,3 @@ app.use(express.static("public"));
 
 let PORT = process.env.PORT || 3000;
 httpServer.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-=======
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-// Define a connection event
-io.on('connection', (socket) => {
-  console.log('User connected');
-
-  // Handle drawing data
-  socket.on('draw', (data) => {
-    // Broadcast the drawing data to all connected clients
-    socket.broadcast.emit('draw', data);
-  });
-
-  // Handle disconnect event
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
->>>>>>> origin/main
